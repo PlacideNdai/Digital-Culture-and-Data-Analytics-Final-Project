2ed0c02d1934bfa2029b2f51e5dc4a78685a2f5e
Placide <75864037+PlacideNdai@users.noreply.github.com>
2026-04-27 20:53:52 -0500

    worked on break down info

--- src/pages/Breakdown.vue
+++ src/pages/Breakdown.vue
@@ -5,6 +5,24 @@
         </div>
 
         <div class="content">
+            <p>
+                Forza has this feature that’s off by default, but when active, you gain more points. It’s my favorite,
+                but I’d hate it sometimes if I am trying to win. The feature simulates a real-world breakdown of a car
+                when you’re driving it aggressively. Meaning when you’re hitting objects as you race, your car will
+                break down, and in turn, the car slows down, and if continued, the car will stop completely. It turns
+                out that this isn’t new. Race Driver Grid, a racing game where the player can race in different
+                continents, has it, and the game is from 2008. As you drive and hit the walls, the car breaks down, and
+                eventually it explodes. Race Driver Grid is a little tough on this compared to Forza.
+            </p>
+
+            <p>
+                There isn’t perfection in anything, that’s intentional by design if you ask me. So, when drifting in and
+                out, it’s okay to tap and break down; what matters is when you get up and continue the journey started.
+                Setbacks and obstacles make us stronger; therefore, don’t run from them.
+            </p>
+
+            <h3>Try Race Driver Grid here:</h3>
+            <a href="https://retrogamesonline.io/race-driver-grid-europe-enfrdeesit"> Play on Retrogamesonline!</a>
         </div>
     </div>
 </template>
@@ -14,16 +32,28 @@
 </script>
 
 <style scoped>
-
 .breakdown-container {
     display: flex;
     flex-direction: column;
     width: 75%;
     margin: auto;
     gap: 1rem;
-    background: white;
+    background: transparent;
+    backdrop-filter: blur(70px);
     padding: .5rem;
     border-radius: 10px;
-    height: 35vh;
+    height: auto;
+    color: white;
+}
+
+.content p{
+    padding: 1rem;
+}
+
+.content a {
+    color: white;
+    font-size: x-large;
+    text-align: center;
+    display: block;
 }
 </style>
