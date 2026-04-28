d8224857b1dd71ff151ff5340a8ce2d5a0169480
Placide <75864037+PlacideNdai@users.noreply.github.com>
2026-04-27 20:24:44 -0500

    finished writing for Arena

--- src/pages/Arena.vue
+++ src/pages/Arena.vue
@@ -5,7 +5,26 @@
         </div>
 
         <div class="content">
+            <p>
+                Forza has many arenas. The world itself is an arena, the mini-games inside are their own arena, and the
+                garage and mechanic shops are also arenas. For this project, the focus will be on the track or
+                battlefield where cars fight until one comes out victorious. Forza has many mini-games, and one of them
+                is about tagging the other players out. The concept is easy: one team tags the other, the player, while
+                they run away. Once tagged, the tagged member joins the tagging team. This idea is from a wrestling game
+                called WWF War Zone USA. In WWF, you choose how many players to play, then they fight in teams until
+                there is one winner. Now, that’s what happens in that arena.
+            </p>
 
+
+            <p>
+                Zooming out for a big view and connection to the theme. When racing or speeding down in Forza and
+                everything is chaotic, that’s the arena you’re facing. The goal is not trying to get out defeated;
+                You’re to drift your way to victory, even if that means getting to point B.
+
+            </p>
+
+            <h3>Try WWF War Zone USA here:</h3>
+            <a href="https://retrogamesonline.io/wwf-war-zone-usa ">Play on Retrogamesonline!</a>
         </div>
     </div>
 </template>
@@ -21,11 +40,22 @@
     width: 75%;
     margin: auto;
     gap: 1rem;
-    background: white;
+    background: transparent;
     padding: .5rem;
     border-radius: 10px;
-    height: 35vh;
+    height: auto;
+    backdrop-filter: blur(70px);
+    color: white;
 }
 
+.content p{
+    padding: 1rem;
+}
 
+.content a {
+    color: white;
+    font-size: x-large;
+    text-align: center;
+    display: block;
+}
 </style>
